36915033e9aba11fd2309fb457607b46ef404cad
[buse duran] <buseduran500@gmail.com>
2023-09-13 14:37:51 +0300

    added new api

--- app_.js
+++ app_.js
@@ -3,13 +3,14 @@ const request = require('request');
 const options = {
     method: 'GET',
     url: 'https://yahoo-weather5.p.rapidapi.com/weather',
+    json: true,
     qs: {
         location: 'sunnyvale',
         format: 'json',
         u: 'c'
     },
     headers: {
-        //apikey
+        //key
         'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
     }
 };
@@ -17,7 +18,29 @@ const options = {
 request(options, function (error, response, body)
 {
     if (error) throw new Error(error);
-    const weatherData = JSON.parse(body)
+    //const weatherData = JSON.parse(body)
+    console.log(body.current_observation.condition.text + '. It is currently ' + body.current_observation.condition.temperature + ' degrees out there. The speed of the wind is ' + body.current_observation.wind.speed + ' km/h.')
+});
 
-    console.log(weatherData.current_observation.condition.text + '. It is currently ' + weatherData.current_observation.condition.temperature + ' degrees out there. The speed of the wind is ' + weatherData.current_observation.wind.speed + ' km/h.')
+const geocodeOptions = {
+    method: 'GET',
+    url: 'https://trueway-geocoding.p.rapidapi.com/Geocode',
+    json: true,
+    qs: {
+        address: '505 Howard St, San Francisco',
+        language: 'en'
+    },
+    headers: {
+        //key
+        'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
+    }
+};
+
+request(geocodeOptions, function (error, response, body)
+{
+    if (error) throw new Error(error);
+
+    const latitude = response.body.results[0].location.lat
+    const longitude = response.body.results[0].location.lng
+    console.log(latitude, longitude)
 });
