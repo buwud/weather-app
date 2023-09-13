ec954a4546364d60f666cf9cdc096bc428febf9d
[buse duran] <buseduran500@gmail.com>
2023-09-13 23:13:02 +0300

    callback functions

--- app_.js
+++ app_.js
@@ -5,42 +5,60 @@ const options = {
     url: 'https://yahoo-weather5.p.rapidapi.com/weather',
     json: true,
     qs: {
-        location: 'sunnyvale',
+        location: 'kayseri',
         format: 'json',
         u: 'c'
     },
     headers: {
-        //key 1
+        //key
         'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
     }
 };
 
 request(options, function (error, response, body)
 {
-    if (error) throw new Error(error);
-    //const weatherData = JSON.parse(body)
-    console.log(body.current_observation.condition.text + '. It is currently ' + body.current_observation.condition.temperature + ' degrees out there. The speed of the wind is ' + body.current_observation.wind.speed + ' km/h.')
+    if (error) //for basic os errors
+    {
+        console.log('Unable to connect to weather service!')
+    } else if (response.body.error)
+    {
+        console.log('unable to find the location')
+    } else
+    {
+        console.log(body.current_observation.condition.text + '. It is currently ' + body.current_observation.condition.temperature + ' degrees out there in ' + options.qs.location + '. The speed of the wind is ' + body.current_observation.wind.speed + ' km/h.')
+    }
+
+    console.log(body)
 });
 
+
+//Geocode**********
 const geocodeOptions = {
     method: 'GET',
     url: 'https://trueway-geocoding.p.rapidapi.com/Geocode',
     json: true,
     qs: {
-        address: '505 Howard St, San Francisco',
+        address: 'Kayseri Turkey',
         language: 'en'
     },
     headers: {
-        //key 2
+        //key
         'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
     }
 };
 
 request(geocodeOptions, function (error, response, body)
 {
-    if (error) throw new Error(error);
-
-    const latitude = response.body.results[0].location.lat
-    const longitude = response.body.results[0].location.lng
-    console.log(latitude, longitude)
+    if (error)
+    {
+        console.log('Unable to connect to the server!')
+    } else if (response.body.results.length === 0)
+    {
+        console.log('Unable to find the location!')
+    } else
+    {
+        const latitude = response.body.results[0].location.lat
+        const longitude = response.body.results[0].location.lng
+        console.log(latitude, longitude)
+    }
 });
