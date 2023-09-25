856f0d89e2377b00c9fa3b50060e6ba0cf545023
[buse duran] <buseduran500@gmail.com>
2023-09-25 13:35:31 +0300

    handlebars to dynamic pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -9,9 +9,33 @@ console.log(path.join(__dirname, '../public'))
 const app = express()
 const publicDir = path.join(__dirname, '../public')
 
+
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(publicDir)))//root
 
+app.get('', (req, res) =>
+{
+    res.render('index', {
+        title: 'weather',
+        author: 'buse duran'
+    })
+})
 
+app.get('/about', (req, res) =>
+{
+    res.render('about', {
+        title: 'about',
+        author: 'buwu duran'
+    })
+})
+
+app.get('/help', (req, res) =>
+{
+    res.render('help', {
+        title: 'help message',
+        author: 'buse'
+    })
+})
 
 app.get('/weather', (req, res) =>
 {
