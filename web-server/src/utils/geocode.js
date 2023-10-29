65ac4f878520f624e7e2271a205f9c68c8b7252e
Buse Duran <buseduran500@gmail.com>
2023-10-29 22:33:39 +0300

    feat: clear button

---
+++ web-server/src/utils/geocode.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const key = require('./key')
+
+const geocode = (address, callback) => {
+    const geocodeOptions = {
+        method: 'GET',
+        url: 'https://trueway-geocoding.p.rapidapi.com/Geocode',
+        json: true,
+        qs: {
+            address: address,
+            language: 'en'
+        },
+        headers: {
+            'X-RapidAPI-Key': key.getKey(),
+            'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
+        }
+    };
+
+    request(geocodeOptions, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to the server!', undefined)
+        } else if (body.results.length === 0) {
+            callback('Unable to find the location! Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.results[0].location.lat,
+                longitude: body.results[0].location.lng,
+                address: body.results[0].address,
+
+            })
+        }
+    })
+}
+module.exports = geocode
